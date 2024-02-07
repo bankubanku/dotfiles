a8e8620e1827b97fd9e08f7cb303e9d289b76265
bankubanku <wozniczka.jakub@proton.me>
2024-02-07 19:22:43 +0100

    polished look of settings.lua

--- nvim/lua/banku/settings.lua
+++ nvim/lua/banku/settings.lua
@@ -1,65 +1,32 @@
--- Set highlight on search
-vim.opt.hlsearch = false
+local options = {
+  hlsearch = false, -- Set highlight on search
+  incsearch = true, -- shows the matches for the current search pattern as you type it 
+  mouse = 'a', -- Enable mouse mode
+  clipboard = 'unnamedplus', -- Sync clipboard between OS and Neovim.
+  undofile = true, -- Save undo history
+  ignorecase = true,  -- Case-insensitive searching 
+  smartcase = true, -- case-sensitive searching when capital letter typed 
+  signcolumn = 'yes', -- this column next to the line number column
+  updatetime = 250, -- saves buffer to swap after given time
+  timeoutlen = 300, -- set amount of time to wait for a key code sequence to complete 
+  completeopt = 'menuone,noselect', -- changes completion experience
+  termguicolors = true, -- checks host terminal's capability and enables 24-bit RGB color
+  number = true, -- adds number column on the left 
+  relativenumber = true, -- makes number column relative
+  cursorline = true,   -- highlight the line where cursor is currently positioned 
+  cursorlineopt = 'number,line', -- what to highlight
+  scrolloff = 10,   -- sets up number of lines to keep above and below the cursor when scrolling 
+  smartindent = true,   -- neovim analyzes the code and controls indentation based on the syntax 
+  expandtab = true, -- spaces instead of tab character
+  tabstop = 4, -- width of the tab character 
+  shiftwidth = 4, -- determines the size of indentation when using `<<`, `>>' or automatic features like autoindent or smartindent
+  softtabstop = 4, -- controls the bahavior of the tab key in insert mode when 'expandtab' is enabled 
+  autoindent = true,  -- automaticaly indents the indentation level of current line when you start a new line 
+  breakindent = true,  -- maintains the indentation level, even if the new line is wrapped 
+  wrap = true,  -- wraps line when it is no longer visible in neovim window 
+  linebreak = true,  -- controls wrapping based on breakat option (only specified character) 
+}
 
--- shows the matches for the current search pattern as you type it 
-vim.opt.incsearch = true
-
--- Enable mouse mode
-vim.opt.mouse = 'a'
-
--- Sync clipboard between OS and Neovim.
-vim.opt.clipboard = 'unnamedplus'
-
--- Save undo history
-vim.opt.undofile = true
-
--- Case-insensitive searching 
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
--- this column next to the line number column
-vim.opt.signcolumn = 'yes'
-
--- saves buffer to swap after given time
-vim.opt.updatetime = 250
-
--- set amount of time to wait for a key code sequence to complete 
-vim.opt.timeoutlen = 300
-
--- changes completion experience
-vim.opt.completeopt = 'menuone,noselect'
-
--- checks host terminal's capability and enables 24-bit RGB color
--- NOTE: You should make sure your terminal supports this
-vim.opt.termguicolors = true
-
--- enabling the column of number on the left 
-vim.opt.number = true
-vim.opt.relativenumber = true
-
--- highlight the line where cursor is currently positioned 
-vim.opt.cursorline = true
-vim.opt.cursorlineopt = 'number,line'
-
--- sets up number of lines to keep above and below the cursor when scrolling 
-vim.opt.scrolloff = 10
-
--- neovim analyzes the code and controls indentation based on the syntax 
-vim.opt.smartindent = true
-
--- size of indentation
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.softtabstop = 4
-
--- automaticaly indents the indentation level of current line when you start a new line 
-vim.opt.autoindent = true
-
--- maintains the indentation level, even if the new line is wrapped 
-vim.opt.breakindent = true
-
--- wraps line when it is no longer visible in neovim window 
-vim.opt.wrap = true
-
--- controls wrapping based on breakat option (only specified character) 
-vim.opt.linebreak = true
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
