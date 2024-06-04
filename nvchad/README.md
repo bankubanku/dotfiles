c2e39b7966498248d0b8dd749946cc7d94921a46
bankubanku <wozniczka.jakub@proton.me>
2024-06-04 17:13:23 +0200

    added updated configuration script for arch linux and renamed nvim config folder

--- nvchad/README.md
+++
@@ -1,2 +0,0 @@
-# my nvchad config 
-I got tired of customizing my own neovim and decided to try out [NvChad](https://nvchad.com/). 
---
+++ script.sh
@@ -0,0 +1,58 @@
+#! /bin/bash
+
+# for this to work base base-devel git linux linux-firmware linux-headers must be installed 
+echo "This configuration uses dependencies from aur. If you want to avoid using software from AUR, reconsider changing this configuration for your needs :3"
+read -p "Do you still want to continue? (y/n): " answer
+
+if [ "$answer != "y" || "$answer != "Y" ]; then 
+    exit 1
+fi
+
+sudo pacman -Syyu
+sudo pacman -S rsync gvim alacritty bluez bluez-utils flameshot man-db nemo obsidian obs-studio qtile rofi signal-desktop syncthing ttf-jetbrains-mono-nerd ufw zip unzip wget noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra virtualbox lightdm lightdm-gtk-greeter pipewire pipewire-audio pipewire-alsa pipewire-pulse dosfstools efibootmgr gimp github-cli htop inkscape libreoffice-still mtools neovim nodejs npm p7zip python-dbus-next python-pip python-pipx python-psuutil ripgrep rofi signal-desktop skanlite syncthing sysfsutils tree ttf-roboto-slab lutris xclip   
+echo "packages from official arch repo installed"
+
+amd=`cat /proc/cpuinfo | grep -i amd`
+intel=`cat /proc/cpuinfo | grep -i intel`
+if [ ${#amd} > 0 ]; then
+    sudo pacman -S amd-ucode
+    echo "amd-ucode installed"
+elif [ ${#intel} > 0 ]; then
+    sudo pacman -S intel-ucode
+    echo "intel-ucode installed"
+else 
+    echo "Couldn't detect machine's cpu information. Install manually amd-ucode, intel-ucode or resolve your problem in other way"
+fi 
+
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+rm -rf yay
+echo "yay built"
+
+yay -S activitywatch-bin betterlockscreen deadd-notification-center-bin freetube librewolf-bin qtile-extras ungoogled-chromium-bin vesktop-bin 
+echo "packages from AUR installed"
+
+sudo systemctl enable lightdm
+sudo systemctl enable bluetooth
+
+pwd=`pwd`
+
+mv ~/.config/alacritty ~/.config/_alacritty
+mv ~/.config/nvim/  ~/.config/_nvim/
+mv ~/.config/qtile/config.py ~/.config/qtile/_config.py
+mv ~/.config/rofi/config.rasi  ~/.config/rofi/_config.rasi
+mv ~/.config/deadd/ ~/.config/_dead
+echo "backed up config files"
+
+ln -s ${pwd}/dotfiles/alacritty ~/.config/alacritty
+ln -s ${pwd}/dotfiles/deadd ~/.config/deadd/
+ln -s ${pwd}/dotfiles/nvim ~/.config/nvim
+ln -s ${pwd}/dotfiles/qtile/config.py ~/.config/qtile/config.py
+ln -s ${pwd}/dotfiles/rofi/config.rasi ~/.config/rofi/config.rasi
+ln -s ${pwd}/dotfiles/rofi/catppuccin-frappe.rasi ~/.local/share/rofi/themes/catppuccin-frappe.rasi
+echo "created symbolic links for config files"
+
+echo "now your OS will reboot"
+sudo reboot
